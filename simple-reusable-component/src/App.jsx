592c9486b6f38d471e1c4880cb30887501b39467
Rabia Zia Nezami <rabianezami786@gmail.com>
2026-01-03 15:17:32 +0430

    Use props in App.jsx

--- simple-reusable-component/src/App.jsx
+++ simple-reusable-component/src/App.jsx
@@ -1,11 +1,16 @@
-import { useState } from 'react'
+
 import './App.css'
+import Badge from './components/Badge'
 
 function App() {
 
   return (
     <>
-    
+     <h1>My Badges</h1>
+     <Badge label="course" value="React" />
+     <Badge label="Weak" value="1" />
+     <Badge label="Topic" value="Props" />
+     <Badge label="Practice" value="Reuseable Components" />
     </>
   )
 }
